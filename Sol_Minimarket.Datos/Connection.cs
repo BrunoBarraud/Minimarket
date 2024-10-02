6892e2b018f46262b658ca8ec5b8a2ddedce7c00
BrunoBarraud <brunobarraud15@gmail.com>
2024-10-02 17:58:56 -0300

    Agregar archivos de proyecto.

---
+++ Sol_Minimarket.Datos/Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Sol_Minimarket.Datos
+{
+    public class Connection
+    {
+        public string DataBase;
+        public string User;
+        public string Password;
+        public static Connection con = null;
+    }
+
+}
